df35c84bde11864ca34e6f8ab033e5cd34d05934
Denis Yersov <frontsteron@gmail.com>
2022-12-08 09:40:33 +0500

    Completed task 1

---
+++ src/main/java/org/example/Project_10/Task1.java
@@ -0,0 +1,23 @@
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+package org.example.Project_10;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print("Введите n: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int sum = 0;
+        int factorial = 1;
+
+        for (int index = 1; index <= n; index++) {
+            sum += index;
+            factorial *= index;
+        }
+
+        System.out.println("Сумма чисел от 1 до n: " + sum);
+        System.out.println("Произведение чисел от 1 до n: " + factorial);
+    }
+}
