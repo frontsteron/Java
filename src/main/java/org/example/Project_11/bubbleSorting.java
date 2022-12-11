f28294a3cf4492a25f35f6d15ea770e60548372a
Denis Yersov <frontsteron@gmail.com>
2022-12-11 10:34:14 +0500

    The task 2 is almost done

---
+++ src/main/java/org/example/Project_11/bubbleSorting.java
@@ -0,0 +1,31 @@
+package org.example.Project_11;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.io.PrintWriter;
+
+public class bubbleSorting {
+    public static void main(String[] args) {
+        int[] array = new int[12];// Задаем случайный массив
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random() * 50) - 25);
+            System.out.println(array[i]);
+        };
+        // Сортируем пузырьком
+        int temp;
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        try(PrintWriter out = new PrintWriter("bubbleSort.txt")) {
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
