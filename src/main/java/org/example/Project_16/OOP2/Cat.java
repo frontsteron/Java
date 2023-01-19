8244bf0a46dc530b490add08f0b2f5c767c05b1f
Denis Yersov <frontsteron@gmail.com>
2023-01-19 10:37:06 +0500

    Completed the task

---
+++ src/main/java/org/example/Project_16/OOP2/Cat.java
@@ -0,0 +1,15 @@
+package org.example.Project_16.OOP2;
+
+final class Cat {
+    private final String name;
+    private final int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void sayHi() {
+        System.out.printf("Кошку звать %s и ей %d лет", this.name, this.age);
+    }
+
+}
---
+++ src/main/java/org/example/Project_16/OOP2/Main.java
@@ -0,0 +1,9 @@
+package org.example.Project_16.OOP2;
+
+public class Main {
+    public static void main(String[] args) {
+        var myCat = new Cat("Муся", 7);
+
+        myCat.sayHi();
+    }
+}
